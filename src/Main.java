af1bf63b9a8dc791c883b8b0b5288559796cd503
Murad <ashrafov.main@gmail.com>
2024-10-04 17:20:37 +0400

    ADDed codes

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("ello world!");
         int x = 5;
-        int y = 5 + 4;
+        int y = x + 4;
         System.out.println(y);
     }
 }
